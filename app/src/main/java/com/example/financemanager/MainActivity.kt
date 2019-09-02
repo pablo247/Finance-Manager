64f96e015dee816a1cc42ed8e998ce303ef443b6
Pablo De Jesús Moreno <pablo.24726@gmail.com>
2019-09-02 01:07:42 -0500

    Cambio del Nav Bottom y SVG's
    Se añade libreria para implementar un nuevo diseño de Nav Bottom
    Se reduce el tamaño de SVG's a 30dp
    Se cambia el color dddde los SVG's al color primario

--- app/src/main/java/com/example/financemanager/MainActivity.kt
+++ app/src/main/java/com/example/financemanager/MainActivity.kt
@@ -2,29 +2,55 @@ package com.example.financemanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import com.example.financemanager.FragmentsNav.FragmentAccounts
 import com.example.financemanager.FragmentsNav.FragmentReports
 import com.example.financemanager.FragmentsNav.FragmentSettings
 import com.example.financemanager.FragmentsNav.FragmentTransactions
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.iammert.library.readablebottombar.ReadableBottomBar
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), ReadableBottomBar.ItemSelectListener {
+
+    private var bottomNavBar : ReadableBottomBar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        main_bottom_nav.setOnNavigationItemSelectedListener(this)
+        bottomNavBar = findViewById(R.id.bottomBar)
+        bottomNavBar!!.setOnItemSelectListener(this)
 
         if (savedInstanceState == null) {
             val fragment = FragmentTransactions()
             supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName).commit()
         }
+
+        /*main_bottom_nav.setOnNavigationItemSelectedListener(this)*/
     }
 
-    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+    override fun onItemSelected(index: Int) {
+        // bottomNavBar!!.selectItem(index)
+        when (index) {
+            0 -> {
+                val fragment = FragmentTransactions()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName).commit()
+            }
+            1 -> {
+                val fragment = FragmentReports()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName).commit()
+            }
+            2 -> {
+                val fragment = FragmentAccounts()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName).commit()
+            }
+            3 -> {
+                val fragment = FragmentSettings()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName).commit()
+            }
+        }
+    }
+
+    /*override fun onNavigationItemSelected(p0: MenuItem): Boolean {
         when (p0.itemId) {
             R.id.menu_transactions -> {
                 val fragment = FragmentTransactions()
@@ -44,6 +70,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
         }
         return true
-    }
+    }*/
 
 }
