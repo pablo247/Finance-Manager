ed81b0182f9b8d7b8991c4eb6db967584fb202de
Pablo De Jesus Moreno <pablo.24726@gmail.com>
2019-09-13 10:44:16 -0500

    Se añade attributo de formato de fecha al DatePicker personalizado.

--- app/src/main/java/com/example/financemanager/components/datePicker/DatePicker.kt
+++ app/src/main/java/com/example/financemanager/components/datePicker/DatePicker.kt
@@ -6,9 +6,12 @@ import android.view.LayoutInflater
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import com.example.financemanager.R
 import java.text.SimpleDateFormat
 import java.util.*
+
+
 
 class DatePicker
     @JvmOverloads constructor(context: Context, attrs: AttributeSet, defStyle: Int = 0, defStyleRes: Int = 0)
@@ -22,12 +25,21 @@ class DatePicker
     private var nextMonth: ImageButton? = null
     private var prevMonth: ImageButton? = null
     private val calendar: Calendar = Calendar.getInstance()
+    private var format: String = ""
 
     private var changeDatePicker: ChangeDatePickerListener? = null
 
     init {
         LayoutInflater.from(this.context).inflate(R.layout.date_picker, this, true)
         orientation = VERTICAL
+
+        attrs?.let {
+            val typedArray = this.context.obtainStyledAttributes(it, R.styleable.date_picker_attributes, 0, 0)
+            this.format = resources.getString(typedArray.getResourceId(R.styleable.date_picker_attributes_format_date, R.string.date_picker_format))
+            // this.format = typedArray.getString(R.styleable.date_picker_attributes_format_date)
+            Toast.makeText(this.context, this.format, Toast.LENGTH_SHORT).show()
+            typedArray.recycle()
+        }
 
         textDate = findViewById(R.id.TextDate)
         nextMonth = findViewById(R.id.ButtonNextMonth)
@@ -47,7 +59,7 @@ class DatePicker
     }
 
     private fun getCurrentDate():String {
-        return SimpleDateFormat("yyy, MMM").format(calendar.time)
+        return SimpleDateFormat(this.format.toString()).format(calendar.time)
     }
 
     private fun changeMonth(typeChange: Int): String {
