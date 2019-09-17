57253d153b4d5c5520d2d67e98a34caea8b43ddc
Pablo De Jesus Moreno <pablo.24726@gmail.com>
2019-09-17 14:02:22 -0500

    Spinner para cambiar la previsualización de las transacciones.

--- app/src/main/java/com/example/financemanager/fragmentsNav/FragmentTransactions.kt
+++ app/src/main/java/com/example/financemanager/fragmentsNav/FragmentTransactions.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import com.example.financemanager.R
 import com.example.financemanager.components.datePicker.DatePicker
@@ -14,10 +17,13 @@ import java.util.*
 /**
  * A simple [Fragment] subclass.
  */
-class FragmentTransactions : Fragment(), DatePicker.ChangeDatePickerListener {
+class FragmentTransactions : Fragment(), DatePicker.ChangeDatePickerListener,
+    AdapterView.OnItemSelectedListener {
 
     private var datePicker: DatePicker? = null
     private var calendar: Calendar? = null
+
+    private var chooseTransactionDisplay: Spinner? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,12 +35,24 @@ class FragmentTransactions : Fragment(), DatePicker.ChangeDatePickerListener {
         datePicker = view.findViewById(R.id.date_picker)
         datePicker!!.setOnChangeDatePickerListener(this)
 
+        chooseTransactionDisplay = view.findViewById(R.id.spinner_choose_transactions_display)
+        val adapter = ArrayAdapter.createFromResource(this.context!!, R.array.choose_transactions_display, R.layout.spinner_layout)
+        adapter.setDropDownViewResource(R.layout.spinner_item_layout)
+        chooseTransactionDisplay!!.adapter = adapter
+
+        chooseTransactionDisplay!!.onItemSelectedListener = this
+
         return view
     }
 
     override fun onChangeDatePicker(calendar: Calendar) {
         this.calendar = calendar
-        Toast.makeText(this.context, this.calendar!!.time.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        Toast.makeText(this.context, "Fragmento $p2 de transacciones", Toast.LENGTH_SHORT).show()
     }
 
 }
